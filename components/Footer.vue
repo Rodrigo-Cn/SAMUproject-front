b07b00ec285c30fbe46eab5a50efbe37fd606a29
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-01-23 18:22:52 -0300

    feat: Adiciona Footer do projeto

---
+++ components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <!--Start footer-->
+    <footer class="text-center bg-body-tertiary">
+            <!-- Grid container -->
+            <div class="container pt-4">
+                <!-- Section: Social media -->
+                <section class="mb-4">
+                    <!-- Facebook -->
+                    <a data-mdb-ripple-init class="btn btn-link btn-floating btn-lg text-body m-1" href="#!"
+                        role="button" data-mdb-ripple-color="white"><i class="zmdi zmdi-home my-icon zmdi-hc-2x"></i></a>
+
+                    <!-- Linkedin -->
+                    <a data-mdb-ripple-init class="btn btn-link btn-floating btn-lg text-body m-1" href="#!"
+                        role="button" data-mdb-ripple-color="white"><i class="zmdi zmdi-linkedin my-icon zmdi-hc-2x"></i></a>
+                    
+                    <!-- Github -->
+                    <a data-mdb-ripple-init class="btn btn-link btn-floating btn-lg text-body m-1" href="#!"
+                        role="button" data-mdb-ripple-color="white"><i class="zmdi zmdi-github my-icon zmdi-hc-2x"></i></a>
+                </section>
+                <!-- Section: Social media -->
+            </div>
+
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+                © 2025 Copyright:
+                <a id="about" href="">Sobre Nós</a>
+            </div>
+        </footer>
+    <!--End footer-->
+</template>
+<script>
+
+</script>
+<style scoped>
+    #about{
+        color: rgb(190, 190, 190);
+    }
+    .my-icon{
+        color: white;
+    }
+</style>
