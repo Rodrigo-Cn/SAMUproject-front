70cc24305917a23b66dc02112d8388f98b77e2e2
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-01-23 18:23:17 -0300

    feat: Adiciona component de login

---
+++ components/FormLogin.vue
@@ -0,0 +1,35 @@
+<template>
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmailId" class="sr-only">Email ID</label>
+                <div class="position-relative has-icon-right">
+                    <input type="text" id="exampleInputEmailId" class="form-control input-shadow"
+                        placeholder="Usuário" />
+                    <div class="form-control-position">
+                        <i class="icon-user"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword" class="sr-only">Password</label>
+                <div class="position-relative has-icon-right">
+                    <input type="password" id="exampleInputPassword" class="form-control input-shadow"
+                        placeholder="Senha" />
+                    <div class="form-control-position">
+                        <i class="icon-lock"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-6"></div>
+                <div class="form-group col-6 text-right"></div>
+            </div>
+            <button type="button" class="btn btn-light btn-block">
+                Entrar
+            </button>
+            <div class="text-center mt-3">Seja bem-vindo!</div>
+        </form>
+</template>
+<script>
+
+</script>
