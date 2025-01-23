0e9df27e6fe610d62d0e3011a555fd26f5742bd7
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-01-23 18:22:23 -0300

    feat: Adiciona navbar e header

---
+++ components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <header class="topbar-nav">
+        <nav class="navbar navbar-expand fixed-top">
+            <ul class="navbar-nav mr-auto align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link toggle-menu" href="javascript:void();">
+                        <i class="icon-menu menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                </li>
+            </ul>
+
+            <ul class="navbar-nav align-items-center right-nav-link">
+                <li class="nav-item dropdown-lg">
+                    <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown">
+                        <i class="fa fa-envelope-open-o"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
+                        <span class="user-profile">
+                            <i class="fa fa-user"></i>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li class="dropdown-item user-details">
+                            <a href="javaScript:void();">
+                                <div class="media">
+                                    <div class="media-body">
+                                        <h6 class="mt-2 user-title">Sarajhon Mccoy</h6>
+                                        <p class="user-subtitle">mccoy@example.com</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="dropdown-item">
+                            <i class="icon-settings mr-2"></i>Editar Conta
+                        </li>
+                        <li class="dropdown-divider"></li>
+                        <li class="dropdown-item">
+                            <i class="icon-power mr-2"></i>Sair
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+<style scoped>
+.dropdown-item{
+    cursor: pointer;
+}
+</style>
---
+++ components/NavBarAdministrator.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
+            <div class="brand-logo">
+                <a class="nav-link toggle-menu" href="javascript:void();">
+                    <img src="public/assets/images/image.png" class="logo-icon" alt="logo icon" />
+                    <h5 class="logo-text">Saúde Integrada</h5>
+                </a>
+            </div>
+            <ul class="sidebar-menu do-nicescrol">
+                <li class="sidebar-header">Página de Navegação</li>
+                <li>
+                    <a href="">
+                        <i class="zmdi zmdi-home"></i> <span>Página Inicial</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="tables.html">
+                        <i class="zmdi zmdi-grid"></i> <span>Pacientes</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="tables.html">
+                        <i class="zmdi zmdi-grid"></i> <span>Médicos</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="formsAtendimento.html">
+                        <i class="zmdi zmdi-format-list-bulleted"></i>
+                        <span>Registrar Atendimento</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="formsPaciente.html">
+                        <i class="zmdi zmdi-face"></i> <span>Cadastrar Paciente</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="register.html" target="_blank">
+                        <i class="zmdi zmdi-account-circle"></i>
+                        <span>Registar Médico</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="calendar.html">
+                        <i class="zmdi zmdi-calendar-check"></i> <span>Mapa</span>
+                        <small class="badge float-right badge-light"></small>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="login.html" target="_blank">
+                        <i class="zmdi zmdi-lock"></i> <span>Login</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-header">Cadastro</li>
+                <li>
+                    <a href="s"><i class="zmdi zmdi-plus-circle text-danger"></i>
+                        <span>Cadastrar Médico</span></a>
+                </li>
+                <li>
+                    <a href="s"><i class="zmdi zmdi-hospital-alt text-danger"></i>
+                        <span>Cadastrar Medicamento</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
